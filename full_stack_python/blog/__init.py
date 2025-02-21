18aaf8e8f722c1fc6d30544d8c316cf5f4475fc1
Tunde4real <ib4real914@gmail.com>
2025-02-21 18:01:34 +0100

    added state for blog

--- full_stack_python/blog/__init.py
+++ full_stack_python/blog/__init.py
@@ -1,5 +1,5 @@
 from .model import BlogPostModel
 
 __all__ = [
-    "BlogPostModel",
+    'BlogPostModel',
 ]
--- full_stack_python/blog/state.py
+++ full_stack_python/blog/state.py
@@ -1,11 +1,20 @@
 from typing import List, Optional
+from sqlmodel import select
+
 
 import reflex as rx
-
 from .model import BlogPostModel
 
 class BlogPostState(rx.State):
     posts: List['BlogPostModel'] = []
     post: Optional['BlogPostModel'] = None
 
-    
+    def load_posts(self):
+        with rx.session() as session:
+            result = session.exec(
+                select(BlogPostModel)
+            )
+            self.posts = result.all()
+
+    # def get_post(self, post_id):
+    #     pass
